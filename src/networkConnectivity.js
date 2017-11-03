b9bae9d023b4e4543dea532d658332af7959a675
Aaron Rice <adrice727@gmail.com>
2017-11-03 15:27:22 -0700

    Update options.

--- src/networkConnectivity.js
+++ src/networkConnectivity.js
@@ -2,13 +2,23 @@
 import publisherTest from './publisherTest';
 import { validateCallbacks } from './validation';
 import { InvalidSessionCredentialsError } from './errors';
+import { getOrElse } from './util';
 
 class NetworkConnectivity {
-  constructor(credentials) {
+  /**
+   * @param {Object} credentials
+   * @param {String} credentials.apiKey
+   * @param {String} credentials.sessionId
+   * @param {String} credentials.token
+   * @param {Object} [options]
+   * @param {Object} [options.environment] - 'standard' or 'enterprise'
+   */
+  constructor(credentials, options = null) {
     if (!credentials || credentials.apiKey || !credentials.sessionId || !credentials.token) {
       throw new InvalidSessionCredentialsError();
     }
     this.credentials = credentials;
+    this.environment = getOrElse('standard', 'environment', options);
   }
 
   checkConnectivity(onStatus, onComplete) {
@@ -16,9 +26,17 @@ class NetworkConnectivity {
     console.log(this.credentials);
   }
 
-  testPublishing(onStatus, onComplete) {
+  /**
+   * @param {Object} [deviceOptions]
+   * @param {String} [deviceOptions.audioSource]
+   * @param {String} [deviceOptions.videoSource]
+   * @param {Function} onStatus
+   * @param {Function} onComplete
+   * @returns {Promise} <resolve: Object, reject: NetworkConnectivityError>
+   */
+  testPublishing(deviceOptions, onStatus, onComplete) {
     validateCallbacks(onStatus, onComplete);
-    publisherTest(this.credentials, onStatus, onComplete);
+    publisherTest(this.credentials, Object.assign({}, this.options, deviceOptions), onStatus, onComplete);
   }
 }
 
