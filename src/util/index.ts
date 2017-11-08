a3dba298b806e4480dfff8b9c8759c4849f9ef02
Aaron Rice <adrice727@gmail.com>
2017-11-08 13:16:53 -0800

    Add 'assoc' and 'assocPath' to Util.

--- src/util/index.ts
+++ src/util/index.ts
@@ -2,6 +2,32 @@
  * @module Util
  */
 
+
+/**
+ * Returns a copy of an object, setting or overriding the property with the provided value
+ */
+const assoc = (key: string, value: any, obj: Object): Object => ({ ...obj, [key]: value });
+
+
+/**
+ * Returns a copy of an object, setting or overriding the property at the specified path
+ * with the provided value.  The path should be provided as a period-delimited string.
+ */
+const assocPath = (path: string, value: any, obj: Object): Object => {
+  const keys: string[] = path.split('.');
+  const key = keys[0];
+  if (!keys.length) {
+    return obj;
+  } else if (keys.length === 1) {
+    return assoc(key, value, obj);
+  } else {
+    const valForKey = get(key, obj);
+    const base: Object = (!!valForKey && typeof valForKey === 'object') ? valForKey : { ...obj, [key]: {} };
+    const update = assoc(key, assocPath(keys.slice(1).join('.'), value, get(key, base)), obj);
+    return { ...obj, ...update };
+  }
+};
+
 /**
  * Returns a (nested) property from the provided object or undefined
  */
@@ -19,7 +45,7 @@ const get = <T>(props: string, obj: any): T => {
  * Returns a (nested) property from the provided object or the default
  * value if undefined
  */
-const getOrElse = <T>(defaultValue: any, props: string, obj: any): T => get(props, obj) || defaultValue;
+const getOr = <T>(defaultValue: any, props: string, obj: any): T => get(props, obj) || defaultValue;
 
 /**
  * Returns a subset of the provided object with the specified properties. Keys whose corresponding
@@ -44,7 +70,7 @@ const pickAll = <T extends { [key: string]: any }, K extends keyof T>(props: K[]
 
 export {
   get,
-  getOrElse,
+  getOr,
   pick,
   pickAll,
 };
