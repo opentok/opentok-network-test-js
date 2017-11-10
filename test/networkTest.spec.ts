6cf22855549ef5178f2561ae2ff11ee68239de0f
Aaron Rice <adrice727@gmail.com>
2017-11-10 10:37:13 -0800

    Add 'toBeInstanceOf' custom matcher.

--- test/networkTest.spec.ts
+++ test/networkTest.spec.ts
@@ -1,13 +1,29 @@
+/* tslint:disable */
 ///<reference path="../src/types/index.d.ts"/>
-
 
 import {
   InvalidSessionCredentialsError,
   MissingOpenTokInstanceError,
   MissingSessionCredentialsError,
 } from '../src/networkTest/errors';
-import networkTest from '../src/networkTest';
 import * as OT from '@opentok/client';
+import NetworkTest from '../src/NetworkTest';
+
+type Util = jasmine.MatchersUtil;
+type CustomMatcher = jasmine.CustomMatcher;
+type EqualityTesters = jasmine.CustomEqualityTester[];
+
+const customMatchers: jasmine.CustomMatcherFactories = {
+  toBeInstanceOf: (util: Util, customEqualityTesters: EqualityTesters): CustomMatcher => {
+    return {
+      compare: (actual: any, expected: any): jasmine.CustomMatcherResult => {
+        const pass: boolean = actual instanceof expected;
+        const message: string = pass ? '' : `Expected ${actual} to be an instance of ${expected}`;
+        return { pass, message };
+      },
+    };
+  },
+};
 
 describe('Network Test Contructor', () => {
 
@@ -23,11 +39,14 @@ describe('Network Test Contructor', () => {
     token: '1234',
   };
 
+  beforeAll(() => jasmine.addMatchers(customMatchers));
+
   it('requires OT and valid session credentials', () => {
-    expect(() => new networkTest(validCredentials)).toThrow(new MissingOpenTokInstanceError());
-    expect(() => new networkTest(OT)).toThrow(new MissingSessionCredentialsError());
-    expect(() => new networkTest(OT, malformedCredentials)).toThrow(new InvalidSessionCredentialsError());
-    // expect(new networkTest(OT, validCredentials)).toThrow(new InvalidSessionCredentialsError());
+    expect(() => new NetworkTest(validCredentials)).toThrow(new MissingOpenTokInstanceError());
+    expect(() => new NetworkTest({}, validCredentials)).toThrow(new MissingOpenTokInstanceError());
+    expect(() => new NetworkTest(OT)).toThrow(new MissingSessionCredentialsError());
+    expect(() => new NetworkTest(OT, malformedCredentials)).toThrow(new InvalidSessionCredentialsError());
+    expect(new NetworkTest(OT, validCredentials)).toBeInstanceOf(NetworkTest);
   });
 
 });
