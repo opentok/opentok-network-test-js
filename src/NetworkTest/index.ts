98f17b5e799588e6f2d8c369e52427cd1898d4fa
Aaron Rice <adrice727@gmail.com>
2017-11-10 14:56:11 -0800

    Rename NetworkTest dir.

---
+++ src/NetworkTest/index.ts
@@ -0,0 +1,84 @@
+/**
+ * @module NetworkTest
+ */
+
+/**
+* Define Network Connectivy class
+*/
+
+import { connectivityTest, ConnectivityTestResults } from './connectivityTest';
+import testQuality from './testQuality';
+import {
+  IncompleteSessionCredentialsError,
+  InvalidOnCompleteCallback,
+  MissingOpenTokInstanceError,
+} from './errors';
+import { getOr } from '../util';
+
+export default class NetworkTest {
+
+  credentials: SessionCredentials;
+  environment: OpenTokEnvironment;
+  OT: OpenTok;
+
+  /**
+   * Returns an instance of NetworkConnectivity
+   */
+  constructor(OT: OpenTok, credentials: SessionCredentials, options?: { environment: OpenTokEnvironment }) {
+    this.validateOT(OT);
+    this.validateCredentials(credentials);
+    this.OT = OT;
+    this.credentials = credentials;
+    this.environment = getOr('standard', 'environment', options);
+  }
+
+  private validateOT(OT: OpenTok) {
+    if (!OT || typeof OT !== 'object' || !OT.initSession) {
+      throw new MissingOpenTokInstanceError();
+    }
+  }
+
+  private validateCredentials(credentials: SessionCredentials) {
+    if (!credentials || !credentials.apiKey || !credentials.sessionId || !credentials.token) {
+      throw new IncompleteSessionCredentialsError();
+    }
+  }
+
+  private validateCallbacks(
+    updateCallback: UpdateCallback<any> | null,
+    onComplete?: CompletionCallback<any>) {
+    if (updateCallback) {
+      if (typeof updateCallback !== 'function' || updateCallback.length !== 1) {
+        throw new InvalidOnUpdateCallback();
+      }
+    }
+    if (onComplete) {
+      if (typeof onComplete !== 'function' || onComplete.length !== 2) {
+        throw new InvalidOnCompleteCallback();
+      }
+    }
+  }
+
+  /**
+   * This function runs a test publisher and based on the measured video bitrate,
+   * audio bitrate, and the audio packet loss for the published stream, it returns
+   * results indicating the recommended supported publisher settings.
+   */
+  testQuality(
+    updateCallback: UpdateCallback<any>,
+    completionCallback: CompletionCallback<any>): Promise<any> {
+    this.validateCallbacks(updateCallback, completionCallback);
+    return testQuality(
+      this.OT, this.credentials, this.environment, updateCallback, completionCallback);
+  }
+
+  /**
+   * This method checks to see if the client can connect to TokBox servers required for using OpenTok
+   */
+  checkConnectivity(
+    deviceOptions?: DeviceOptions,
+    onComplete?: CompletionCallback<any>): Promise<ConnectivityTestResults> {
+    this.validateCallbacks(null, null, onComplete);
+    return connectivityTest(this.OT, this.credentials, this.environment, deviceOptions, onComplete);
+  }
+}
