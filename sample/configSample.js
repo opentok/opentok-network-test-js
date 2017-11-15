223ee84af82f8986e58cf0ba7f376ec31a4b4886
Jeff Swartz <swartz@tokbox.com>
2017-11-14 16:25:48 -0800

    Adding the sample directory
    This got clobbered inadvertantly by a previous PR.

---
+++ sample/configSample.js
@@ -0,0 +1,5 @@
+module.exports = {
+  apiKey: '123456', // Add your own OpenTok API key here. See the README in this directory.
+  sessionId: '1_MX40NzIwMzJ-fjE1MDElGQkJJfn4', // Add your own session ID here
+  token: 'T1==cGFydG5lcXN0PQ==' // Add your own token here
+}
---
+++ sample/index.js
@@ -0,0 +1,40 @@
+/* global require */
+
+import OTNetworkTest from 'opentok-network-test-js';
+var OTNetworkTestOptions = require('./config.js');
+
+console.log('sdfsdfsdf', OTNetworkTestOptions)
+var otNetworkTest = new OTNetworkTest(OT, OTNetworkTestOptions);
+
+setStatus('Testing connectivity');
+otNetworkTest.checkConnectivity(function(error, results) {
+  console.log('checkConnectivity callback error', error);
+  console.log('checkConnectivity callback results', results);
+}).then(function(results) {
+  console.log('checkConnectivity promise results', results);
+  testQuality();
+}).catch(function(error) {
+  console.log('checkConnectivity promise error', error);
+});
+
+function testQuality() {
+  setStatus('Testing quality.');
+  otNetworkTest.testQuality(function updateCallback(stats) {
+    console.log('testQuality updateCallback', stats);
+  }).then(function(results) {
+    console.log('testQuality promise results', results);
+    setStatus('Quality test done.');
+  }).catch(function(error) {
+    console.log('testQuality promise error', error);
+  });
+}
+
+function setStatus(statusText) {
+  var statusMessageEl = document.getElementById('status_container').querySelector('p');
+
+  if (statusMessageEl.textContent) {
+    statusMessageEl.textContent = statusText;
+  } else if (statusMessageEl.innerText) {
+    statusMessageEl.innerText = statusText;
+  }
+}
