b81da75ad274d92bc712482abea9156f2ab6c597
Aaron Rice <adrice727@gmail.com>
2017-11-15 15:20:03 -0800

    Rename warnings.

--- src/warnings/index.ts
+++ src/warnings/index.ts
@@ -6,26 +6,26 @@
   * Base class for warnings used throughout Network Connectivity tests.
   */
 
-export class NetworkConnectivityWarning {
+export class NetworkTestWarning {
   message: string;
   constructor(message: string) {
     this.message = message;
   }
 }
 
-export class AudioDeviceNotAvailableWarning extends NetworkConnectivityWarning {
+export class AudioDeviceNotAvailableWarning extends NetworkTestWarning {
   constructor(id: string){
     super(`The audio device with id ${id} does not exist. Falling back to default device.`);
   }
 }
 
-export class VideoDeviceNotAvailableWarning extends NetworkConnectivityWarning {
+export class VideoDeviceNotAvailableWarning extends NetworkTestWarning {
   constructor(id: string){
     super(`The video device with id ${id} does not exist. Falling back to default device.`);
   }
 }
 
-export class FailedToConnectToLoggingServer extends NetworkConnectivityWarning {
+export class FailedToConnectToLoggingServer extends NetworkTestWarning {
   constructor(){
     super(`Failed to connect to the OpenTok logging server.`);
   }
