a195273ac82180b106354c79d29b7c10d874b718
Aaron Rice <adrice727@gmail.com>
2017-11-16 17:52:04 -0800

    Add test setup, basic tests for connectivity test.

--- test/NetworkTest.spec.ts
+++ test/NetworkTest.spec.ts
@@ -10,12 +10,22 @@ import {
   InvalidOnCompleteCallback,
   InvalidOnUpdateCallback,
 } from '../src/networkTest/errors';
+import { pick } from '../src/util';
 import * as OT from '@opentok/client';
 import NetworkTest from '../src/NetworkTest';
-import { escapeExpression } from 'handlebars';
+import * as credentials from './credentials.json';
+import { ConnectivityTestResults } from '../src/NetworkTest/testConnectivity/index';
+
 type Util = jasmine.MatchersUtil;
 type CustomMatcher = jasmine.CustomMatcher;
 type EqualityTesters = jasmine.CustomEqualityTester[];
+
+
+const sessionCredentials = credentials.standard;
+const badCredentials = { apiKey: '1234', invalidProp: '1234', token: '1234' };
+const networkTest = new NetworkTest(OT, sessionCredentials);
+const validOnUpdateCallback = (stats: OT.SubscriberStats) => stats;
+const validOnCompleteCallback = (error?: Error, results?: any) => results;
 
 const customMatchers: jasmine.CustomMatcherFactories = {
   toBeInstanceOf: (util: Util, customEqualityTesters: EqualityTesters): CustomMatcher => {
@@ -27,50 +37,59 @@ const customMatchers: jasmine.CustomMatcherFactories = {
       },
     };
   },
+  toBeABoolean: (util: Util, customEqualityTesters: EqualityTesters): CustomMatcher => {
+    return {
+      compare: (actual: any, expected: any): jasmine.CustomMatcherResult => {
+        const pass: boolean = typeof actual === 'boolean';
+        const message: string = pass ? '' : `Expected ${actual} to be an instance of ${expected}`;
+        return { pass, message };
+      },
+    };
+  },
 };
 
 describe('Network Test', () => {
 
-  const malformedCredentials = {
-    apiKey: '1234',
-    invalidProp: '1234',
-    token: '1234',
-  };
-
-  const validCredentials = {
-    apiKey: '1234',
-    sessionId: '1234',
-    token: '1234',
-  };
-
-  const networkTest = new NetworkTest(OT, validCredentials);
-
-  const validOnUpdateCallback = (stats: OT.SubscriberStats) => stats;
-  const validOnCompleteCallback = (error?: Error, results?: any) => results;
-
-  beforeAll(() => jasmine.addMatchers(customMatchers));
+  beforeAll(() => {
+    jasmine.addMatchers(customMatchers);
+  });
 
   it('its constructor requires OT and valid session credentials', () => {
-    expect(() => new NetworkTest(validCredentials)).toThrow(new MissingOpenTokInstanceError());
-    expect(() => new NetworkTest({}, validCredentials)).toThrow(new MissingOpenTokInstanceError());
+    expect(() => new NetworkTest(sessionCredentials)).toThrow(new MissingOpenTokInstanceError());
+    expect(() => new NetworkTest({}, sessionCredentials)).toThrow(new MissingOpenTokInstanceError());
     expect(() => new NetworkTest(OT)).toThrow(new MissingSessionCredentialsError());
-    expect(() => new NetworkTest(OT, malformedCredentials)).toThrow(new IncompleteSessionCredentialsError());
-    expect(new NetworkTest(OT, validCredentials)).toBeInstanceOf(NetworkTest);
+    expect(() => new NetworkTest(OT, badCredentials)).toThrow(new IncompleteSessionCredentialsError());
+    expect(new NetworkTest(OT, sessionCredentials)).toBeInstanceOf(NetworkTest);
   });
 
   describe('Connectivity Test', () => {
+
     it('validates its onComplete callback', () => {
       expect(() => networkTest.testConnectivity('callback').toThrow(new InvalidOnCompleteCallback()))
       expect(() => networkTest.testConnectivity(validOnCompleteCallback).not.toThrowError(NetworkTestError))
-    })
-  })
+    });
 
-  describe('Quality Test', () => {
-    it('validates its onUpdate and onComplete callbacks', () => {
-      expect(() => networkTest.testQuality('callback').toThrow(new InvalidOnUpdateCallback()))
-      expect(() => networkTest.testQuality(validOnUpdateCallback, 'callback').toThrow(new InvalidOnCompleteCallback()))
-      expect(() => networkTest.testConnectivity(validOnUpdateCallback, validOnCompleteCallback).not.toThrowError(NetworkTestError))
-    })
-  })
+    describe('Test Results', () => {
+      it('should contain success and failedTests properties', (done) => {
+        networkTest.testConnectivity()
+          .then((results: ConnectivityTestResults) => {
+            it('should contain a boolean success property', (done) => {
+              expect(results.success).toBeABoolean
+            });
+            it('should contain an array of failedTests', (done) => {
+              expect(results.failedTests).toBeInstanceOf(Array);
+            });
+            done();
+          });
+      });
+    });
 
+    describe('Quality Test', () => {
+      it('validates its onUpdate and onComplete callbacks', () => {
+        expect(() => networkTest.testQuality('callback').toThrow(new InvalidOnUpdateCallback()))
+        expect(() => networkTest.testQuality(validOnUpdateCallback, 'callback').toThrow(new InvalidOnCompleteCallback()))
+        expect(() => networkTest.testConnectivity(validOnUpdateCallback, validOnCompleteCallback).not.toThrowError(NetworkTestError))
+      })
+    });
+  });
 });
---
+++ test/setup/setup.js
@@ -0,0 +1,38 @@
+const OpenTok = require('opentok');
+const Promise = require('promise');
+const fse = require('fs-extra');
+const config = require('./config');
+
+function createSessionAndToken({ apiKey, apiSecret }) {
+  return new Promise((resolve, reject) => {
+    const opentok = new OpenTok(apiKey, apiSecret);
+    opentok.createSession({ mediaMode: 'routed' }, (error, session) => {
+      if (error) {
+        reject(error);
+      } else {
+        const token = opentok.generateToken(session.sessionId);
+        const { sessionId } = session;
+        resolve({ apiKey, sessionId, token });
+      }
+    });
+  });
+}
+
+function writeCredentials(credentialsArray) {
+  const [ standardCredentials, safariCredentials] = credentialsArray;
+  const credentials = {
+    standard: standardCredentials,
+    safari: safariCredentials,
+  };
+  return fse.outputJson('./test/credentials.json', credentials);
+}
+
+function generateCredentials(){
+  const { standard, safari } = config;
+  Promise.all([standard, safari].map(createSessionAndToken))
+  .then(writeCredentials)
+  .then((results) => console.info('Generated session credentials for test.'))
+  .catch(e => console.error('Failed to generate test credentials', e));
+}
+
+generateCredentials();
