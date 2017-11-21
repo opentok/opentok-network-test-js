4c4c10798e345e9fa691a4dc26c3c7014da5a5c6
Aaron Rice <adrice727@gmail.com>
2017-11-21 13:10:04 -0800

    Remove unused import from webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const nodeExternals = require('webpack-node-externals');
 const TypedocWebpackPlugin = require('typedoc-webpack-plugin');
 
 module.exports = {
