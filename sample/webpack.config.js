d02152cb81fa146b7ecb1c468b79a69cf6dd2352
Jeff Swartz <swartz@tokbox.com>
2018-04-16 15:26:15 -0700

    More changes for IE 11 support

--- sample/webpack.config.js
+++ sample/webpack.config.js
@@ -1,8 +1,19 @@
 module.exports = {
-    entry: "./src/js/index.js",
+    entry: ['babel-polyfill', './src/js/index.js'],
     node: {
       fs: "empty",
       net: "empty"
+    },
+    module: {
+      loaders: [
+          {
+              loader: 'babel-loader',
+              exclude: /node_modules/,
+              query: {
+                  presets: ['env']
+              }
+          }
+      ]
     },
     output: {
         path: __dirname,
