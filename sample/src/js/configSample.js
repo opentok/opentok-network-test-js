7a3a8109ffa35d017ce1f57583a3227ff827f821
Jeff Swartz <jeff.swartz@vonage.com>
2019-12-10 13:09:24 -0800

    Safari no longer requires H.264

--- sample/src/js/configSample.js
+++ sample/src/js/configSample.js
@@ -2,12 +2,4 @@ module.exports = {
   apiKey: '123456', // Add your own OpenTok API key here. See the README in this directory.
   sessionId: '1_MX40NzIwMzJ-fjE1MDElGQkJJfn4', // Add your own session ID here
   token: 'T1==cGFydG5lcXN0PQ==', // Add your own token here
-  // Add h264 settings if you want to support Safari.
-  // Use an OpenTok project with the preferred video codec set to H.264.
-  // See https://tokbox.com/account.
-  h264: {
-    apiKey: '',
-    sessionId: '',
-    token: ''
-  }
 }
