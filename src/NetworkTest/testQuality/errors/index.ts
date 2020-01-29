3fc291787fbead1e35a26eec2fae166c0add1b31
Jeff Swartz <jeff.swartz@vonage.com>
2020-01-29 15:04:50 -0800

    Correcting unsupported browser error message

--- src/NetworkTest/testQuality/errors/index.ts
+++ src/NetworkTest/testQuality/errors/index.ts
@@ -24,7 +24,7 @@ export class QualityTestError extends NetworkTestError {
 export class UnsupportedBrowserError extends QualityTestError {
   constructor(browser: string) {
     const message =
-      `Your current browser (${browser}) does not support the audio-video quality test. Please run the test in Chrome or Firefox.`;
+      `Your current browser (${browser}) does not support the audio-video quality test. Please run the test in a supported browser.`;
     super(message, ErrorNames.UNSUPPORTED_BROWSER);
   }
 }
